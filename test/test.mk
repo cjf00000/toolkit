3794ba3065c600cda061f16afa066019fae87c5d
xunzheng <xunzheng@cs.cmu.edu>
2013-11-24 18:30:28 -0500

    added pthread linking

--- test/test.mk
+++ test/test.mk
@@ -1,8 +1,12 @@
 
+TEST_LIBS = -pthread
+
+# ===================== rules =====================
+
 test_all: test_blocking_queue
 
 $(BIN)/blocking_queue_test: $(TEST)/blocking_queue_test.cpp $(SRC)/blocking_queue.h
-	$(CXX) $(CXXFLAGS) $(DIRFLAGS) $< $(LIBS) -o $@
+	$(CXX) $(CXXFLAGS) $(DIRFLAGS) $< $(LIBS) $(TEST_LIBS) -o $@
 
 test_blocking_queue: $(BIN)/blocking_queue_test
 	LD_LIBRARY_PATH=$(LIB) $<
